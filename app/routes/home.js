1564ad00da99cd084dda9131e1c4da96706808ed
Roberta Jacimbertt de Jesus <robertaj@br.ibm.com>
2018-11-27 12:46:58 -0200

    rota/home

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        app.app.controllers.home.listarMaterias(app, req, res);
+        res.render('home/index');
     });
 }
